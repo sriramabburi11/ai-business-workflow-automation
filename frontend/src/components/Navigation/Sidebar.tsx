a9d6fb5cb09f27830640f9b758c0e258800e11b6
Sriram Abburi <sriramabburi@Srirams-MacBook-Air.local>
2026-08-06 14:24:42 +0530

    Ensure sidebar is permanently fixed/sticky on desktop viewports and off-canvas on mobile

--- frontend/src/components/Navigation/Sidebar.tsx
+++ frontend/src/components/Navigation/Sidebar.tsx
@@ -51,10 +51,10 @@ export const Sidebar: React.FC<SidebarProps> = ({
         />
       )}
 
-      {/* Sidebar Drawer */}
+      {/* Sidebar: Fixed on Desktop (lg:sticky lg:top-0 lg:shrink-0), Drawer on Mobile */}
       <aside
-        className={`fixed top-0 bottom-0 left-0 z-50 h-screen w-64 bg-[#0b0f19] border-r border-slate-800 flex flex-col select-none transition-transform duration-300 ease-in-out lg:static lg:translate-x-0 ${
-          isOpen ? 'translate-x-0 shadow-2xl' : '-translate-x-full'
+        className={`fixed top-0 bottom-0 left-0 z-50 h-screen w-64 bg-[#0b0f19] border-r border-slate-800 flex flex-col select-none transition-transform duration-300 ease-in-out lg:sticky lg:top-0 lg:shrink-0 lg:translate-x-0 ${
+          isOpen ? 'translate-x-0 shadow-2xl' : '-translate-x-full lg:translate-x-0'
         }`}
       >
         {/* Brand Header */}
